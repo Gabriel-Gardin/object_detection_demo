4b7e4b1883c943d4ffd707693e969647f76471df
Gabrie <gabrieltgardin@gmail.com>
2020-07-30 12:03:46 -0300

    testeeee

---
+++ rename_image.py
@@ -0,0 +1,23 @@
+import os
+import glob
+
+
+def rename_all(source):
+    os.chdir(source)
+    all_files = os.listdir(source)
+    files_len = len(all_files)/2
+    i = 0
+    for file in glob.glob("*.jpg"):
+        try:
+            os.rename(file[:-3]+'xml','{}.xml'.format(i))
+            os.rename(file,'{}.jpg'.format(i))
+        except Exception as e:
+            os.remove(file)
+            print(e)
+    
+        i += 1
+    
+
+if __name__ == "__main__":
+    src = "/home/gardin/Documents/dev/RoadDamageDetector-master/RDD2020_data/train_data/images_subset"
+    rename_all(src)
