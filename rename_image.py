7dd3e522c019bae33b6f1d27dc5187ef00a233ae
Gabrie <gabrieltgardin@gmail.com>
2020-08-20 09:56:51 -0300

    test

--- rename_image.py
+++ rename_image.py
@@ -5,19 +5,17 @@ import glob
 def rename_all(source):
     os.chdir(source)
     all_files = os.listdir(source)
-    files_len = len(all_files)/2
     i = 0
-    for file in glob.glob("*.jpg"):
+    for file in glob.glob("*.jpeg"):
         try:
             #os.rename(file[:-3]+'xml','{}.xml'.format(i))
-            os.rename(file,'{}.jpg'.format(i))
+            os.rename(file,'{}.jpg'.format(i+2204))
+            i += 1
         except Exception as e:
             os.remove(file)
             print(e)
     
-        i += 1
-    
 
 if __name__ == "__main__":
-    src = "/home/gardin/Desktop/placas"
+    src = "/home/gardin/Documents/Estagio_LET/let_obj_detection/all_dataset_final"
     rename_all(src)
